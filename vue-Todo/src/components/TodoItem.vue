cf5978da25972533a42a33b9609621221461d36e
hanggukeem <o1b.hannah@gmail.com>
2020-11-05 23:34:13 +0900

    vue-Todo

---
+++ vue-Todo/src/components/TodoItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      {{todo.title}}</p>
+      <button @click="$emit('del-todo', todo.id)" 
+      class="del">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete(){
+      this.todo.completed = !this.todo.completed;
+  }
+  }
+}
+</script>
+<style scoped>
+div {
+  position: relative;
+}
+  .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border: 1px solid #ccc;
+  }
+
+  .is-complete {
+    text-decoration: line-through;
+  }
+
+  .del {
+    background: #ff0000;
+    color: #ffffff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+  position: absolute;
+  right: 0;
+  top: 20px;    
+  }
+</style>
